c12d710f00a20c9c9714a7a6b0fa53e4e89162b2
Pawel Parafianowicz <pawelp@outlook.com>
2017-01-16 16:30:04 -0500

    Calculates year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -7,3 +7,15 @@ name = gets.chomp
 
 # Outputs the name stored in "name"
 puts "Hi #{name}!"
+
+# Asks user their age
+puts "How old are you?"
+
+# Captures users input and saves it as an integer.
+i = gets.chomp.to_i
+
+# Calculates the year the user was born
+year_born = 2017 - i
+
+# Displays the year the user was born
+puts "You were born in #{year_born}"
